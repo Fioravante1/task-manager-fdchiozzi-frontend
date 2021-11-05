e50b3ab03c3bf8e4b3f8aa908d2869bf619e89bf
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-11-04 22:13:57 -0300

    Feat: Cria componente home/page

---
+++ src/pages/HomeListTasks.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListTasks from '../components/listTasks/ListTasks';
+
+function HomeListTasks() {
+  return (
+    <ListTasks />
+  );
+}
+
+export default HomeListTasks;
